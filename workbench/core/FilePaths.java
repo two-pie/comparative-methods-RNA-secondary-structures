cdb681dd3ffa56e1e4a83ec63a52c4f2f6acc7d3
Piermuz7 <piermichele.rosati@gmail.com>
2022-08-17 15:18:59 +0100

    implemented cluster group for csv core1 and core2 files

--- workbench/core/FilePaths.java
+++ workbench/core/FilePaths.java
@@ -1,14 +1,13 @@
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public interface FilePaths {
 
     Path root = Paths.get(System.getProperty("user.dir"));
-    Path Archaea  = Paths.get( root.toString(),"Molecules-pseudoknotfree", "db-nH", "Archaea", "5S");
-    Path Bacteria = Paths.get( root.toString(),"Molecules-pseudoknotfree", "db-nH", "Bacteria", "5S");
-    Path Eukaryota = Paths.get( root.toString(),"Molecules-pseudoknotfree", "db-nH", "Eukaryota", "5S");
-    Path Archaea_output = Paths.get( root.toString(),"Molecules-pseudoknotfree", "db-nH", "Archaea", "5S", "Archaea_cores.csv");
-    Path Bacteria_output = Paths.get( root.toString(),"Molecules-pseudoknotfree", "db-nH", "Bacteria", "5S", "Bacteria_cores.csv");
-    Path Eukaryota_output = Paths.get( root.toString(),"Molecules-pseudoknotfree", "db-nH", "Eukaryota", "5S", "Eukaryota_cores.csv");
+    Path Archaea = Paths.get(root.toString(), "workbench", "Molecules-pseudoknotfree", "db", "Archaea", "5S");
+    Path Bacteria = Paths.get(root.toString(), "workbench", "Molecules-pseudoknotfree", "db", "Bacteria", "5S");
+    Path Eukaryota = Paths.get(root.toString(), "workbench", "Molecules-pseudoknotfree", "db", "Eukaryota", "5S");
+    Path Archaea_output = Paths.get(root.toString(), "workbench", "workbench-results", "Archaea", "Archaea");
+    Path Bacteria_output = Paths.get(root.toString(), "workbench", "workbench-results", "Bacteria", "Bacteria");
+    Path Eukaryota_output = Paths.get(root.toString(), "workbench", "workbench-results", "Eukaryota", "Eukaryota");
 }
--- workbench/core/MainComparator.java
+++ workbench/core/MainComparator.java
@@ -1,39 +1,18 @@
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * MainComparator class interacting with the user through command line
- * options.
+ * MainComparator class interacting with the user through command line options.
  *
  * @author Luca Tesei
+ *
  */
 public class MainComparator {
 
-
     public static void main(String[] args) throws IOException {
-        createCsv(Files.walk(FilePaths.ARCHAEA).filter(Files::isRegularFile).collect(Collectors.toList()), FilePaths.ARCHAEA_OUTPUT);
-        createCsv(Files.walk(FilePaths.BACTERIA).filter(Files::isRegularFile).collect(Collectors.toList()), FilePaths.BACTERIA_OUTPUT);
-        createCsv(Files.walk(FilePaths.EUKARYOTA).filter(Files::isRegularFile).collect(Collectors.toList()), FilePaths.EUKARYOTA_OUTPUT);
+        ClusterGroup.createCsvCores(Files.walk(FilePaths.Archaea).filter(Files::isRegularFile).collect(Collectors.toList()), FilePaths.Archaea_output);
+        ClusterGroup.createCsvCores(Files.walk(FilePaths.Bacteria).filter(Files::isRegularFile).collect(Collectors.toList()), FilePaths.Bacteria_output);
+        ClusterGroup.createCsvCores(Files.walk(FilePaths.Eukaryota).filter(Files::isRegularFile).collect(Collectors.toList()), FilePaths.Eukaryota_output);
     }
-
-    private static void createCsv(List<Path> filePaths, Path file_output) throws IOException {
-        Files.write(file_output, "MOLECULE,CORE1,CORE2\n".getBytes());
-        for (var f : filePaths) {
-            var rnaSecondaryStructure = RNASecondaryStructureFileReader.readStructure(f.toString(), false);
-            var fileName = f.getFileName().toString();
-            String csvRow = fileName.substring(0,fileName.lastIndexOf('.')) +
-                    "," +
-                    new RNACore1(rnaSecondaryStructure).getBrackets() +
-                    "," +
-                    new RNACore2(rnaSecondaryStructure).getBrackets() +
-                    "\n";
-            Files.write(file_output, csvRow.getBytes(), StandardOpenOption.APPEND);
-        }
-    }
-
 }
