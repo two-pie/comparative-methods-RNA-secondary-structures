28d0972c47ac76bcc381fd600f3daed7f868a9d1
Piero <68665714+xPieroxk@users.noreply.github.com>
2022-09-08 23:05:28 +0200

    added treegraph tool

--- workbench/workbench.py
+++ workbench/workbench.py
@@ -1,49 +1,36 @@
 import subprocess
 from paths import *
 
-
-def read_files(cores_folder, distances_folder):
-    cores = os.listdir(cores_folder)
-    distances = os.listdir(distances_folder)
-    for c in cores:
-        for d in distances:
-            subprocess.run(
-                ['python3', 'ClusterMatrix.py', os.path.join(cores_folder, c), os.path.join(distances_folder, d)])
-
-
 if __name__ == '__main__':
     # Distance calculator
-
     # Archaea-90-110-allType
     subprocess.run(['aspralign_distance_tool', DBNFilesNH, result_AT_aspralign])
     subprocess.run(['dualgraph_distance_tool', CTFilesNH, result_AT_dualgraph])
     subprocess.run(['nestedalign_distance_tool', DBNFilesNH, result_AT_nestedalign])
     subprocess.run(['rnadistance_distance_tool', DBNFilesNH, result_AT_rnadistance])
     subprocess.run(['rnaforester_distance_tool', DBNFilesNH, result_AT_rnaforester])
+    subprocess.run(['treegraph_distance_tool', CTFilesNH, result_AT_treegraph])
 
     # Molecules-pseudoknotfree
     # Archaea
     subprocess.run(['aspralign_distance_tool', DBNFilesNH, result_MP_aspralign_Archaea])
-    subprocess.run(['dualgraph_distance_tool', CTFilesNH, result_MP_dualgraph_Archaea])
-    subprocess.run(['nestedalign_distance_tool', DBNFilesNH, result_MP_nestedalign_Archaea])
-    subprocess.run(['rnadistance_distance_tool', DBNFilesNH, result_MP_rnadistance_Archaea])
+    subprocess.run(['dualgraph_distance_tool', ctnh_archaea, result_MP_dualgraph_Archaea])
+    subprocess.run(['nestedalign_distance_tool', dbnh_archaea, result_MP_nestedalign_Archaea])
+    subprocess.run(['rnadistance_distance_tool', dbnh_archaea, result_MP_rnadistance_Archaea])
     subprocess.run(['rnaforester_distance_tool', DBNFilesNH, result_MP_rnaforester_Archaea])
-
+    subprocess.run(['treegraph_distance_tool', ctnh_archaea, result_MP_treegraph_Archaea])
     # Bacteria
     subprocess.run(['aspralign_distance_tool', DBNFilesNH, result_MP_aspralign_Bacteria])
-    subprocess.run(['dualgraph_distance_tool', CTFilesNH, result_MP_dualgraph_Bacteria])
-    subprocess.run(['nestedalign_distance_tool', DBNFilesNH, result_MP_nestedalign_Bacteria])
-    subprocess.run(['rnadistance_distance_tool', DBNFilesNH, result_MP_rnadistance_Bacteria])
+    subprocess.run(['dualgraph_distance_tool', ctnh_bacteria, result_MP_dualgraph_Bacteria])
+    subprocess.run(['nestedalign_distance_tool', dbnh_bacteria, result_MP_nestedalign_Bacteria])
+    subprocess.run(['rnadistance_distance_tool', dbnh_bacteria, result_MP_rnadistance_Bacteria])
     subprocess.run(['rnaforester_distance_tool', DBNFilesNH, result_MP_rnaforester_Bacteria])
+    subprocess.run(['treegraph_distance_tool', ctnh_bacteria, result_MP_treegraph_Bacteria])
 
     # Eukaryota
     subprocess.run(['aspralign_distance_tool', DBNFilesNH, result_MP_aspralign_Eukaryota])
-    subprocess.run(['dualgraph_distance_tool', CTFilesNH, result_MP_dualgraph_Eukaryota])
-    subprocess.run(['nestedalign_distance_tool', DBNFilesNH, result_MP_nestedalign_Eukaryota])
-    subprocess.run(['rnadistance_distance_tool', DBNFilesNH, result_MP_rnadistance_Eukaryota])
+    subprocess.run(['dualgraph_distance_tool', ctnh_eukaryota, result_MP_dualgraph_Eukaryota])
+    subprocess.run(['nestedalign_distance_tool', dbnh_eukaryota, result_MP_nestedalign_Eukaryota])
+    subprocess.run(['rnadistance_distance_tool', dbnh_eukaryota, result_MP_rnadistance_Eukaryota])
     subprocess.run(['rnaforester_distance_tool', DBNFilesNH, result_MP_rnaforester_Eukaryota])
-
-    # Clustering
-    '''molecule_files = os.listdir(WORKBENCH_RESULTS)
-    for f in molecule_files:
-        read_files(os.path.join(WORKBENCH_RESULTS, f, 'cores'), os.path.join(WORKBENCH_RESULTS, f, 'distances'))'''
+    subprocess.run(['treegraph_distance_tool', ctnh_eukaryota, result_MP_treegraph_Eukaryota])
